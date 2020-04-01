508dff3619c3f301b05a16743ee2cdc5211b78ef
Wilson <p780729@hotmail.com>
2020-04-02 01:06:37 +0800

    Fix the bug for execute middleware 2 times

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// 要求，設計一個 middleware ，能在終端機印出請求的 HTTP 方法，網址(url)以及時間戳記
+// middleware 實作，設計一個 middleware ，能在終端機印出請求的 HTTP 方法，網址(url)以及時間戳記
 // ex:
 // 2019-5-17 18:51:12 | GET from /
+
+// 進階題：留下兩次時間戳記，兩次的戳記分別需為：
+// (1) 收到請求 (request)，以及(2) 送出回應 (response) 的時間。
+// ex:
+// 2019-5-17 18:51:12 | GET from /
+// 2019-5-17 18:51:12 | GET from / | total time: 8ms
 
 const express = require('express')
 const app = express()
@@ -9,28 +15,25 @@ const port = 3000
 // middleware
 const checkTimeMiddleware = function (req, res, next) {
   const start = new Date().getTime()
+
+  showPassTime(start)
+
+  let receiveReqTime = new Date()
+  receiveReqTime = receiveReqTime.toISOString().slice(0, 10) + receiveReqTime.toString().slice(15, 25)
+
+  console.log('reqTime |', receiveReqTime, '|', req.method, 'path from', req.route.path)
+  console.log('reqTime |', receiveReqTime, '|', req.method, 'url  from', req.url)
+
   function showPassTime(startTime) {
     setTimeout(() => {
       let sendResTime = new Date()
       sendResTime = sendResTime.toISOString().slice(0, 10) + sendResTime.toString().slice(15, 25)
-      let endTime = new Date().getTime()
-      let passTime = endTime - startTime
-      console.log('res', 'resTime', sendResTime, '| total time: ', passTime, 'ms')
+      const endTime = new Date().getTime()
+      const passTime = endTime - startTime
+      console.log('resTime |', sendResTime, '| total time: ', passTime, 'ms')
+      console.log('')
     }, 0)
   }
-  showPassTime(start)
-
-  let receiveReqTime = new Date()
-  let checkUrl = ''
-  // console.log('toISOString', receiveReqTime.toISOString())
-  // console.log('toDateString', receiveReqTime.toISOString())
-  // console.log(receiveReqTime.toString().slice(16, 25))
-  receiveReqTime = receiveReqTime.toISOString().slice(0, 10) + receiveReqTime.toString().slice(15, 25)
-  checkUrl += req.rawHeaders.filter(item => {
-    if (item.includes('http')) { return true }
-  })
-
-  console.log('req', 'reqTime', receiveReqTime, '|', req.method, 'from', checkUrl)
 
   next()
 }
@@ -38,25 +41,25 @@ const checkTimeMiddleware = function (req, res, next) {
 // 列出全部 Todo
 app.get('/', checkTimeMiddleware, (req, res) => {
   res.send('列出全部 Todo')
-  console.log('列出全部 Todo res後')
 })
 
 // 新增一筆 Todo 頁面
-app.get('/new', checkTimeMiddleware, (req, res) => {
+app.get('/todos/new', checkTimeMiddleware, (req, res) => {
   res.send('新增 Todo 頁面')
 })
 
 // 顯示一筆 Todo 的詳細內容
-app.get('/:id', checkTimeMiddleware, (req, res) => {
+app.get('/todos/:id', checkTimeMiddleware, (req, res) => {
   res.send('顯示一筆 Todo')
 })
 
 // 新增一筆  Todo
-app.post('/', checkTimeMiddleware, (req, res) => {
+app.post('/todos/', checkTimeMiddleware, (req, res) => {
   res.send('新增一筆  Todo')
 })
 
-app.delete('/:id/delete', checkTimeMiddleware, (req, res) => {
+// 刪除一筆 Todo
+app.delete('/todos/:id/delete', checkTimeMiddleware, (req, res) => {
   res.send('刪除 Todo')
 })
 
