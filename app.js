cf763e95a9ff7ae019f1416439157c348f8a869b
Wilson Tseng <p780729@hotmail.com>
2020-04-23 17:08:34 +0800

    Try to fix the error of passTime

--- app.js
+++ app.js
@@ -12,54 +12,49 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// middleware
-const checkTimeMiddleware = function (req, res, next) {
-  const start = new Date().getTime()
+var checkTimeMiddleware = function (req, res, next) {
+  const startHrTime = process.hrtime()
 
-  showPassTime(start)
-
-  let receiveReqTime = new Date()
-  receiveReqTime = receiveReqTime.toISOString().slice(0, 10) + receiveReqTime.toString().slice(15, 25)
-
-  console.log('reqTime |', receiveReqTime, '|', req.method, 'path from', req.route.path)
-  console.log('reqTime |', receiveReqTime, '|', req.method, 'url  from', req.url)
-
-  function showPassTime(startTime) {
-    setTimeout(() => {
-      let sendResTime = new Date()
-      sendResTime = sendResTime.toISOString().slice(0, 10) + sendResTime.toString().slice(15, 25)
-      const endTime = new Date().getTime()
-      const passTime = endTime - startTime
-      console.log('resTime |', sendResTime, '| total time: ', passTime, 'ms')
-      console.log('')
-    }, 0)
-  }
+  res.on('finish', () => {
+    const elapsedHrTime = process.hrtime(startHrTime)
+    const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6
+    console.log('%s : %fms', req.path, elapsedTimeInMs)
+  })
 
   next()
+  // req.requestTime = new Date()
+  // req.requestTime = req.requestTime.toISOString().slice(0, 10) + req.requestTime.toString().slice(15, 25)
+  // console.log('reqTime |', req.requestTime, '|', req.method, 'path from', req.route.path)
+  // next()
 }
 
+app.use(checkTimeMiddleware)
+
 // 列出全部 Todo
-app.get('/', checkTimeMiddleware, (req, res) => {
+app.get('/', (req, res) => {
+  for (let i = 1; i < 100000; i++) {
+    
+  }
   res.send('列出全部 Todo')
 })
 
 // 新增一筆 Todo 頁面
-app.get('/todos/new', checkTimeMiddleware, (req, res) => {
+app.get('/todos/new', (req, res) => {
   res.send('新增 Todo 頁面')
 })
 
 // 顯示一筆 Todo 的詳細內容
-app.get('/todos/:id', checkTimeMiddleware, (req, res) => {
+app.get('/todos/:id', (req, res) => {
   res.send('顯示一筆 Todo')
 })
 
 // 新增一筆  Todo
-app.post('/todos/', checkTimeMiddleware, (req, res) => {
+app.post('/todos/', (req, res) => {
   res.send('新增一筆  Todo')
 })
 
 // 刪除一筆 Todo
-app.delete('/todos/:id/delete', checkTimeMiddleware, (req, res) => {
+app.delete('/todos/:id/delete', (req, res) => {
   res.send('刪除 Todo')
 })
 
